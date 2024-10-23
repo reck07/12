60f1c270182598da21fab980b46f50d2964ffa13
THE RECKONER <162774479+reck07@users.noreply.github.com>
2024-10-23 20:55:35 +0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const menuOpenButton = document.querySelector("#menu-open-button");
-const menuCloseButton = document.querySelector("#menu-close-button");
-
-menuOpenButton.addEventListener("click", () => {
-    document.body.classList.toggle("show-mobile-menu");
-});
-
-menuCloseButton.addEventListener("click", () =>
-menuOpenButton.click());
